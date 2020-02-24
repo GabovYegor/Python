af1617e790411118b80fdea63fdd7559cf0161e9
GabovYegor <gabov.egor2017@yandex.ru>
2020-02-24 10:04:26 +0300

    NN_1 not done

---
+++ NN_1/main.py
@@ -0,0 +1,22 @@
+import pandas
+from sklearn.preprocessing import LabelEncoder
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.utils import to_categorical
+
+dataframe = pandas.read_csv("iris.csv", header=None)
+dataset = dataframe.values
+X = dataset[:,0:4].astype(float)
+Y = dataset[:,4]
+
+encoder = LabelEncoder()
+encoder.fit(Y)
+encoded_Y = encoder.transform(Y)
+dummy_y = to_categorical(encoded_Y)
+
+model = Sequential()
+model.add(Dense(4, activation='relu'))
+model.add(Dense(3, activation='relu'))
+
+model.compile(optimizer='adam',loss='categorical_crossentropy', metrics=['accuracy'])
+model.fit(X, dummy_y, epochs=75, batch_size=10, validation_split=0.1)
